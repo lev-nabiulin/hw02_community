93bed12acfb3b72dcc20115d70885f9f93ebd007
lev-nabiulin <levnabiulin@yandex.ru>
2021-07-30 00:46:40 +0300

    Who am i?

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Post(models.Model):
     group = models.ForeignKey(
         'Group',
@@ -16,10 +17,11 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
 
-    
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+
     def __str__(self):
         return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,7 +9,7 @@ def index(request):
     }
     return render(request, 'posts/index.html', context)
 
-    
+
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
