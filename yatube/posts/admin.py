873c0749ef8fdfe3191610b24175757ecf7058ab
lev-nabiulin <levnabiulin@yandex.ru>
2021-08-02 13:49:53 +0300

    pepito2.0

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import Post
 
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
     list_editable = ('group',)
